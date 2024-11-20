895e9405c17c0c00e6afc9d59c749d38051fef15
Fedor Babushkin <babushkin@me.com>
2024-11-20 21:13:14 +0400

    JS added

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,7 +1,28 @@
-
-window.onload = function()
-{
-    const initPerson = personGenerator.getPerson();
-    document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+window.onload = function() {
+    makeRandomPerson();
 };
 
+function makeRandomPerson () {
+    let person = personGenerator.getPerson();
+    document.querySelector('#image').style.backgroundImage = `url('${person.avatar}')`;
+    document.querySelector('#name').textContent = `${person.firstName} ${person.middleName} ${person.surname}`;
+    document.querySelector('#gender').textContent = person.gender;
+    document.querySelector('#date').textContent = person.birthDate;
+    document.querySelector('#profession').textContent = person.profession;
+}
+
+function clear () {
+    document.querySelector('#image').style.backgroundImage = null;
+    document.querySelector('#name').textContent = null;
+    document.querySelector('#gender').textContent = null;
+    document.querySelector('#date').textContent = null;
+    document.querySelector('#profession').textContent = null;
+}
+
+document.querySelector('#generate').addEventListener('click', function() {
+    makeRandomPerson();
+});
+
+document.querySelector('#clear').addEventListener('click', function() {
+    clear();
+});
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -1,5 +1,5 @@
 const personGenerator = {
-    surnameJson: `{  
+    surnameJson: `{
         "count": 15,
         "list": {
             "id_1": "Иванов",
@@ -17,8 +17,7 @@ const personGenerator = {
             "id_13": "Степанов",
             "id_14": "Павлов",
             "id_15": "Александров",
-            "id_16": "Морозов"
-        }
+            "id_16": "Морозов"}
     }`,
     firstNameMaleJson: `{
         "count": 10,
@@ -35,6 +34,118 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Ольга",
+            "id_2": "Екатерина",
+            "id_3": "Елизавета",
+            "id_4": "Ирина",
+            "id_5": "Наталья",
+            "id_6": "Анна",
+            "id_7": "Анастасия",
+            "id_8": "Татьяна",
+            "id_9": "Елена",
+            "id_10": "Марина"
+        }
+    }`,
+    middleNameJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Алексеевич",
+            "id_2": "Сергеевич",
+            "id_3": "Константинович",
+            "id_4": "Олегович",
+            "id_5": "Фёдорович",
+            "id_6": "Михайлович",
+            "id_7": "Андреевич",
+            "id_8": "Данилович",
+            "id_9": "Егорович",
+            "id_10": "Петрович"
+        }
+    }`,
+
+    professionMaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Манекен для краш-тестов",
+            "id_2": "Решатель капчи",
+            "id_3": "Пивной сомелье",
+            "id_4": "Телеканал РЕН-ТВ: штатный экзорцист",
+            "id_5": "Акробат - гомеопат",
+            "id_6": "Гений, миллиардер, филантроп",
+            "id_7": "UX/UI дизайнер Facebook (запрещено в РФ)",
+            "id_8": "Разработчик браузера Амиго",
+            "id_9": "Коллекционер плагинов VS Code",
+            "id_10": "Проставитель точки с запятой в коде JS"
+        }
+    }`,
+
+    professionFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Штатный таролог Московской биржи",
+            "id_2": "Распутывательница проводов наушников",
+            "id_3": "Автоответчица в колл-центре",
+            "id_4": "Критик турецких сериалов",
+            "id_5": "Металлургиня",
+            "id_6": "Разрабатывательница феминитивов",
+            "id_7": "Потомственная ведунья",
+            "id_8": "Телеграм бот",
+            "id_9": "PR менеджер Алексея Панина",
+            "id_10": "Тренер личностного упадка"
+        }
+    }`,
+
+    avatarMaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "assets/m1.jpg",
+            "id_2": "assets/m2.jpg",
+            "id_3": "assets/m3.jpg",
+            "id_4": "assets/m4.jpg",
+            "id_5": "assets/m5.jpg",
+            "id_6": "assets/m6.jpg",
+            "id_7": "assets/m7.jpg",
+            "id_8": "assets/m8.jpg",
+            "id_9": "assets/m9.jpg",
+            "id_10": "assets/m10.jpg"
+        }
+    }`,
+
+    avatarFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "assets/w1.jpg",
+            "id_2": "assets/w2.jpg",
+            "id_3": "assets/w3.jpg",
+            "id_4": "assets/w4.jpg",
+            "id_5": "assets/w5.jpg",
+            "id_6": "assets/w6.jpg",
+            "id_7": "assets/w7.jpg",
+            "id_8": "assets/w8.jpg",
+            "id_9": "assets/w9.jpg",
+            "id_10": "assets/w10.jpg"
+        }
+    }`,
+
+    monthOfBirthJson: `{
+        "count": 12,
+        "list": {
+            "id_1": "января",
+            "id_2": "февраля",
+            "id_3": "марта",
+            "id_4": "апреля",
+            "id_5": "мая",
+            "id_6": "июня",
+            "id_7": "июля",
+            "id_8": "августа",
+            "id_9": "сентября",
+            "id_10": "октября",
+            "id_11": "ноября",
+            "id_12": "декабря"
+        }
+    }`,
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -47,24 +158,91 @@ const personGenerator = {
         return obj.list[prop];
     },
 
-    randomFirstName: function() {
+    randomAvatar: function () {
+        if (this.person.gender == 'Мужчина') {
+            return this.randomValue(this.avatarMaleJson);
+        } else {
+            return this.randomValue(this.avatarFemaleJson);
+        }
+    },
 
-        return this.randomValue(this.firstNameMaleJson);
+    randomFirstName: function () {
+
+        if (this.person.gender == 'Мужчина') {
+            return this.randomValue(this.firstNameMaleJson);
+        } else {
+            return this.randomValue(this.firstNameFemaleJson);
+        }
+
+    },
+
+    randomMiddleName: function () {
+
+        if (this.person.gender == 'Мужчина') {
+            return this.randomValue(this.middleNameJson);
+        } else {
+            return this.randomValue(this.middleNameJson).slice(0, -2) + 'на';
+        }
 
     },
 
 
-     randomSurname: function() {
+    randomSurname: function () {
 
-        return this.randomValue(this.surnameJson);
+        if (this.person.gender == 'Мужчина') {
+            return this.randomValue(this.surnameJson);
+        } else {
+            return this.randomValue(this.surnameJson) + 'а';
+        }
+    },
 
+    randomGender: function () {
+
+        return this.randomIntNumber() === 0 ? this.GENDER_MALE : this.GENDER_FEMALE;
+
+    },
+
+    randomDate: function () {
+        const year = this.randomIntNumber(2010, 1990);
+        const month = this.randomValue(this.monthOfBirthJson);
+        let day;
+        if (month == 'апреля' || month == 'июня' || month == 'сентября' || month == 'ноября') {
+            // если один из этих месяцев, значит в нем 30 дней
+            day = this.randomIntNumber(30, 1);
+        } else if (month == 'февраля') {
+            // чтобы определить, сколько дней в феврале, нужно узнать високосный год или нет. Для этого добавляем условие
+            if ((year/4) % 1 === 0) {
+                // если год делится на 4 без остатка, значит он високосный
+                day = this.randomIntNumber(29, 1);
+            } else {
+                day = this.randomIntNumber(28, 1);
+            }
+        } else {
+            // во всех остальных случаях - 31 день.
+            day = this.randomIntNumber(31, 1);
+        }
+
+        return (`${day} ${month} ${year}`);
+    },
+
+    randomProfession: function () {
+        if ((this.person.gender == 'Мужчина')) {
+            return this.randomValue(this.professionMaleJson);
+        } else {
+            return this.randomValue(this.professionFemaleJson);
+        }
     },
 
 
     getPerson: function () {
         this.person = {};
-        // this.person.gender = this.randomGender();
+        this.person.gender = this.randomGender();
+        this.person.avatar = this.randomAvatar();
         this.person.firstName = this.randomFirstName();
+        this.person.middleName = this.randomMiddleName();
+        this.person.surname = this.randomSurname();
+        this.person.birthDate = this.randomDate();
+        this.person.profession = this.randomProfession();
         return this.person;
     }
 };
